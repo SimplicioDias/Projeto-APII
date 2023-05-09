03f89bdedae16d39e891c0ee2efcb7315b9cc7d5
SimplicioDias <simplicio.g3@gmail.com>
2023-05-09 14:20:57 -0300

    iniciando o cadastro

--- base.py
+++ base.py
@@ -1,1 +1,18 @@
-from datetime import datetime
+from datetime import datetime           #import para saber a idade da pessoa com base no ano atual da máquina
+
+cadastro = list()
+pessoa = dict()
+
+print('▬▬▬▬▬▬ Cadastro ▬▬▬▬')          #CABEÇALHO
+pessoa['nome'] = str(input('Nome: '))
+pessoa['idade'] = datetime.now().year - int(input('Ano de nascimento: '))
+pessoa['email'] = str(input('E-mail: '))
+
+
+print('▬▬▬▬▬▬ Cadastro Finalizado ▬▬▬▬▬▬')
+cadastro.append(pessoa.copy())
+pessoa.clear()
+
+
+print(cadastro)     #teste
+print(pessoa)
