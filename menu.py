3c8cf6fd7af5eaec66fa838abce5c31b6a80e767
SimplicioDias <simplicio.g3@gmail.com>
2023-05-23 17:57:25 -0300

    MENUS

--- menu.py
+++ menu.py
@@ -10,3 +10,11 @@ def menuInicial(lista):
     entrada = interface.leiaInt('\033[32mSua Opção: \033[m')
     return entrada
 
+def menuSystem(lista):
+    interface.cabeçalho('<NOME PROJ>')
+    for pos, item in enumerate (lista):
+        print(f'\033[33m{pos+1}\033[33m - \033[34m{item}\033[m')
+    print(interface.linha())
+    entrada = interface.leiaInt('\033[32mSua Opção: \033[m')
+    return entrada
+    
