f7a20901280d9067e6890bcd6e87ea4d11550590
natanaelpc <132955469+natanaelpc@users.noreply.github.com>
2023-06-11 04:24:33 -0300

    Update Projeto.py

--- Projeto.py
+++ Projeto.py
@@ -1,55 +1,113 @@
-cadastros = []
+import sqlite3
+from tkinter import Tk, Label, Entry, Button, Text
+
+def criar_banco_dados():
+    conn = sqlite3.connect('cadastro.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS pessoas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            idade INTEGER,
+            email TEXT,
+            numero TEXT,
+            cidade TEXT,
+            profissao TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
 def cadastrar_pessoa():
-    print("==== Cadastro de Pessoa ====")
-    nome = input("Nome: ")
-    idade = input("Idade: ")
-    email = input("E-mail: ")
-    telefone = input("Telefone: ")
-    endereco = input("Endereço: ")
-    pessoa = {
-        "Nome": nome,
-        "Idade": idade,
-        "Email": email,
-        "Telefone": telefone,
-        "Endereço": endereco
-    }
-    cadastros.append(pessoa)
-    print("Cadastro realizado com sucesso!\n")
+    nome = entry_nome.get()
+    idade = int(entry_idade.get())
+    email = entry_email.get()
+    numero = entry_numero.get()
+    cidade = entry_cidade.get()
+    profissao = entry_profissao.get()
 
-def visualizar_cadastros():
-    print("==== Cadastros ====")
-    if not cadastros:
-        print("Nenhum cadastro encontrado.\n")
+    conn = sqlite3.connect('cadastro.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO pessoas (nome, idade, email, numero, cidade, profissao)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (nome, idade, email, numero, cidade, profissao))
+    conn.commit()
+    conn.close()
+    resultado_text.insert('end', "Pessoa cadastrada com sucesso!\n")
+
+def pesquisar_pessoas_por_profissao():
+    profissao = entry_pesquisa.get()
+
+    conn = sqlite3.connect('cadastro.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+        SELECT nome, idade, email, numero, cidade
+        FROM pessoas
+        WHERE profissao = ?
+    ''', (profissao,))
+    resultados = cursor.fetchall()
+    conn.close()
+
+    if len(resultados) == 0:
+        resultado_text.insert('end', "Nenhum profissional encontrado.\n")
     else:
-        for i, pessoa in enumerate(cadastros, start=1):
-            print(f"Cadastro {i}:")
-            print(f"Nome: {pessoa['Nome']}")
-            print(f"Idade: {pessoa['Idade']}")
-            print(f"E-mail: {pessoa['Email']}")
-            print(f"Telefone: {pessoa['Telefone']}")
-            print(f"Endereço: {pessoa['Endereço']}")
-            print()
+        resultado_text.insert('end', "Profissionais encontrados:\n")
+        for resultado in resultados:
+            nome, idade, email, numero, cidade = resultado
+            resultado_text.insert('end', f"Nome: {nome}\nIdade: {idade}\nEmail: {email}\nNúmero: {numero}\nCidade: {cidade}\n\n")
 
-def exibir_menu():
-    print("==== Menu ====")
-    print("1. Cadastrar pessoa")
-    print("2. Visualizar cadastros")
-    print("3. Encerrar programa")
+# Criação da interface gráfica
+janela = Tk()
+janela.title("Sistema de Cadastro")
+janela.geometry("400x400")
+janela.configure(bg='#222222')
 
-exibir_menu()
+label_nome = Label(janela, text="Nome:", bg='#222222', fg='#ffffff')
+label_nome.pack()
+entry_nome = Entry(janela)
+entry_nome.pack()
 
-while True:
-    opcao = input("Digite o número da opção desejada: ")
+label_idade = Label(janela, text="Idade:", bg='#222222', fg='#ffffff')
+label_idade.pack()
+entry_idade = Entry(janela)
+entry_idade.pack()
 
-    if opcao == "1":
-        cadastrar_pessoa()
-    elif opcao == "2":
-        visualizar_cadastros()
-    elif opcao == "3":
-        print("Encerrando o programa...")
-        break
-    else:
-        print("Opção inválida. Por favor, tente novamente.\n")
+label_email = Label(janela, text="Email:", bg='#222222', fg='#ffffff')
+label_email.pack()
+entry_email = Entry(janela)
+entry_email.pack()
 
-    exibir_menu()
+label_numero = Label(janela, text="Número:", bg='#222222', fg='#ffffff')
+label_numero.pack()
+entry_numero = Entry(janela)
+entry_numero.pack()
+
+label_cidade = Label(janela, text="Cidade:", bg='#222222', fg='#ffffff')
+label_cidade.pack()
+entry_cidade = Entry(janela)
+entry_cidade.pack()
+
+label_profissao = Label(janela, text="Profissão:", bg='#222222', fg='#ffffff')
+label_profissao.pack()
+entry_profissao = Entry(janela)
+entry_profissao.pack()
+
+btn_cadastrar = Button(janela, text="Cadastrar", command=cadastrar_pessoa, bg='#444444', fg='#ffffff')
+btn_cadastrar.pack()
+
+label_pesquisa = Label(janela, text="Pesquisar por profissão:", bg='#222222', fg='#ffffff')
+label_pesquisa.pack()
+entry_pesquisa = Entry(janela)
+entry_pesquisa.pack()
+
+btn_pesquisar = Button(janela, text="Pesquisar", command=pesquisar_pessoas_por_profissao, bg='#444444', fg='#ffffff')
+btn_pesquisar.pack()
+
+resultado_text = Text(janela, bg='#111111', fg='#ffffff')
+resultado_text.pack()
+
+# Criação do banco de dados
+criar_banco_dados()
+
+janela.mainloop()
